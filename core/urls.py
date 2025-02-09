5469211867481a2eb6a4d72f51afb1b663a6937a
Altamar24 <maga99911@mail.ru>
2025-02-09 18:40:27 +0300

    add login view and template

--- core/urls.py
+++ core/urls.py
@@ -24,7 +24,9 @@ urlpatterns = [
 
     # Auth
     path('signup/', views.signupuser, name='signupuser'),
+    path('login/', views.loginuser, name='loginuser'),
     path('logout/', views.logoutuser, name='logoutuser'),
+
 
     # Todos
     path('', views.home, name='home'),
