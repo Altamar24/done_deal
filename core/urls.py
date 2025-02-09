66481557afa39367162a692d6e34d9d8f4e9f2e2
Altamar24 <maga99911@mail.ru>
2025-02-10 00:10:32 +0300

    mini update

--- core/urls.py
+++ core/urls.py
@@ -27,9 +27,9 @@ urlpatterns = [
     path('login/', views.loginuser, name='loginuser'),
     path('logout/', views.logoutuser, name='logoutuser'),
 
-
     # Todos
     path('', views.home, name='home'),
     path('create/', views.createtodo, name='createtodo'),
     path('current/', views.currenttodos, name='currenttodos'),
+    path('todo/<int:todo_pk>', views.viewtodo, name='viewtodo'),
 ]
